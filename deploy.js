36b52654e29b76527077e1f71490397dd06e8056
Evie Salomon <evie@floozyinc.com>
2019-11-14 01:02:59 -0800

    back to nw builder

--- deploy.js
+++ deploy.js
@@ -1,14 +1,15 @@
-const NwBuilder = require('nw-builder-webpack-plugin');
+const NwBuilder = require('nw-builder');
 
 let options = {
   files: ['package.json', 'src/**', 'node_modules/**'],
+
   platforms: ['osx64', 'win32', 'win64', 'linux32', 'linux64'],
   appName: "BackBit Tool",
   flavor: "normal",
   macIcns: "src/icon.icns"
 }
 
-new NwBuilder().build(options).then(function () {
+new NwBuilder(options).build().then(function () {
   console.log('Build succeeded.');
 }).catch(function (error) {
   console.error('Build failed: ', error);
