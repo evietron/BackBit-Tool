31db34dcfc251d0651f8f56e132a6e04decf974a
Evie Salomon <evie@floozyinc.com>
2019-11-14 00:47:44 -0800

    builder works (now need to pack)

--- deploy.js
+++ deploy.js
@@ -1,13 +1,14 @@
-var NwBuilder = require('nw-builder');
-var nw = new NwBuilder({
-  files: ['package.json', 'src/**'],
+const NwBuilder = require('nw-builder-webpack-plugin');
+
+let options = {
+  files: ['package.json', 'src/**', 'node_modules/**'],
   platforms: ['osx64', 'win32', 'win64', 'linux32', 'linux64'],
   appName: "BackBit Tool",
   flavor: "normal",
   macIcns: "src/icon.icns"
-});
+}
 
-nw.build().then(function () {
+new NwBuilder().build(options).then(function () {
   console.log('Build succeeded.');
 }).catch(function (error) {
   console.error('Build failed: ', error);
